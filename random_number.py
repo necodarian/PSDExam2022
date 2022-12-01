1bf8ca39e3f749450a245abc4803fcab3b6eccbb
Neco Darian <necdar49004@stud.noroff.no>
2022-12-02 00:49:38 +0100

    This is the second change on the python code

--- random_number.py
+++ random_number.py
@@ -15,4 +15,13 @@ class RandomNumber:
         Returns:
             A list of numbers(integers) according to the amount
         """
+        a = max_range - min_range
 
+        if amount > a:
+            raise ValueError("The total random number must be lower than the length of range(min_range, max_range)!")
+
+        if max_range > min_range:
+            random_list = random.sample(range(min_range, max_range), amount)
+            print(random_list)
+            print(type(random_list[0]))
+            print(len(random_list))
