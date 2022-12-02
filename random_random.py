825e0cc9cb1f95d7d95d83f3373169c9b17f1606
Neco Darian <necdar49004@stud.noroff.no>
2022-12-02 11:48:33 +0100

    The fully working version of the code

--- random_random.py
+++ random_random.py
@@ -15,13 +15,24 @@ class RandomNumber:
         Returns:
             A list of numbers(integers) according to the amount
         """
-        a = max_range - min_range
+        a = (max_range - min_range)
+        if type(amount) is str:
+            raise ValueError("No valid integer!")
 
-        if amount > a:
-            raise ValueError(f"The total random number must be lower than the length of range({min_range}, {max_range})!")
+        if min_range >= max_range:
+            raise ValueError(f"It is required that {max_range} should be higher than {min_range}!")
 
-        if max_range > min_range:
+        elif amount > a:
+            raise ValueError(f"The total generated numbers must be lower than the length of range({min_range}, {max_range})!")
+
+        else:
             random_list = random.sample(range(min_range, max_range), amount)
             print(random_list)
-            print(type(random_list[0]))
-            print(len(random_list))
+            print(f"The data types of random_list items are {type(random_list[0])}")
+            print(f"The length of random_list {len(random_list)} is lower than the length of the range between min_range and max_range{a}")
+
+
+
+RandomNumber.random_number(5, 1, 20)
+
+
